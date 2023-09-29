c1cc843682ad9145b6e33b34d8d43707e6bf1664
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-29 20:59:03 +0300

    Delicious Mission Passed

---
+++ pizza-app/src/components/NewRestaurantForm.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function NewRestaurant({ onAddRestaurant}){
+    const [name, setName] = useState("")
+    const [address, setAddress] = useState("")
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch("/restaurants", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({
+                name: name,
+                address: address,
+            }),
+        })
+        .then((r) => r.json())
+        .then((newRestaurant) => onAddRestaurant(newRestaurant));
+    }
+
+    return (
+        <div className="new-restaurant-form">
+            <h2> New Restaurant</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                name="name"
+                placeholder="Restaurant Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                type="text"
+                name="address"
+                placeholder="Restaurant Address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                />
+            </form>
+        </div>
+    );
+}
+
+export default NewRestaurant
