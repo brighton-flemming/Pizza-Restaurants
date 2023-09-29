165da38779af6f9e098e6b154f619261f46a6760
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-29 21:18:20 +0300

    Delicious Mission Passed

---
+++ pizza-app/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <h1>Funky Anchovy
+                <span className="logo" role="img">
+                🍕🎨🔥🚴‍♂️🍕
+                </span>
+            </h1>
+        </header>
+    )
+}
+
+export default Header
