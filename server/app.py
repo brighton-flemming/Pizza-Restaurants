e056794f857816000c651211830d895944009dc5
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-10-01 20:05:16 +0300

    Delicious Mission Passed

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_migrate import Migrate
+from models import db
+from routes import create_app
+
+app = create_app()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newdb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+Migrate(app, db)
+
+if __name__ =='__main__':
+    app.run(port=5555,debug=True)
